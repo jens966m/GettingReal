4024157fc4c4e88140384b03fd654f6c4ce0ac8c
Mithrall <kenneth-dorn@hotmail.com>
2016-11-03 12:21:36 +0100

    Begin Project

--- GettingReal/Test.cs
+++ GettingReal/Test.cs
@@ -4,7 +4,10 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace GettingReal {
     [TestClass]
     public class Test {
-
+        [TestMethod]
+        public void CustomerHasAName() {
+            Customer customer = new Customer("Jens");
+            Assert.AreEqual("Jens", customer.Name);
         }
     }
 }
