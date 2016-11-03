1994a7943be8478983ca7335827973b949e374d0
Mithrall <kenneth-dorn@hotmail.com>
2016-11-03 12:23:11 +0100

    Test for "ACustomerCanChooseATime"

--- GettingReal/Test.cs
+++ GettingReal/Test.cs
@@ -2,12 +2,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace GettingReal {
-    [TestClass]
-    public class Test {
-        [TestMethod]
-        public void CustomerHasAName() {
-            Customer customer = new Customer("Jens");
-            Assert.AreEqual("Jens", customer.Name);
-        }
+
     }
 }
