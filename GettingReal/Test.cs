60e62836c5e973fbc49741956a1b9e51950c60ad
Mithrall <kenneth-dorn@hotmail.com>
2016-11-03 15:23:01 +0100

    Test for "ACustomerCanChooseATime"

--- GettingReal/Test.cs
+++ GettingReal/Test.cs
@@ -5,9 +5,18 @@ namespace GettingReal {
     [TestClass]
     public class Test {
         [TestMethod]
-        public void CustomerHasAName() {
-            Customer customer = new Customer("Jens");
+        public void CustomerHaveAName() {
+            Customer customer = new Customer();
+            customer.Name = "Jens";
+
             Assert.AreEqual("Jens", customer.Name);
+        }
+        [TestMethod]
+        public void ACustomerCanChooseATime() {
+            Customer customer = new Customer();
+            customer.Booktime(2016, 12, 24, 12, 00, 00); //hint DateTime      and       ToString("d/M/yyyy HH:mm")
+
+            Assert.AreEqual("24-12-2016 12:00", customer.Time);
         }
     }
 }
