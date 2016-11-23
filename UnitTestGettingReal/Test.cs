71cb98cca95b9237d78fc60dbc6b89cbe107d6cf
Kenneth Damkjær Nielsen <kenn6091@edu.eal.dk>
2016-11-23 12:47:39 +0100

    test

--- UnitTestGettingReal/Test.cs
+++ UnitTestGettingReal/Test.cs
@@ -31,7 +31,7 @@ namespace UnitTestGettingReal {
         }
         [TestMethod]
         public void ACustomerCanChooseATime() {
-           
+           //test
             customer2.BookATime("12", "12", "2016", "12", "00");
 
             Assert.AreEqual("2016-12-12 12:00:00", customer2.Times[0].ToString());
